e822ab3a2c3fc80e980f5648952065f3a62c894b
Francisco Alex <franciscoalexlq@gmail.com>
2020-12-25 21:44:50 -0200

    primeiro commit

---
+++ Estrutura sequencial/exercicio03.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+número1 = float(input('Digite um número: '))
+número2 = float(input('Digite um outro número: '))
+
+print(f'A soma entre {número1} e {número2} = {número1 + número2}')
