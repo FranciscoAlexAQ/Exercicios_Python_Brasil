d40be04f29b1ebd94569a74b67ae68bc7c72a736
Francisco Alex <franciscoalexlq@gmail.com>
2020-12-25 22:08:12 -0200

    ex 04 a 05

---
+++ Estrutura sequencial/exercicio05.py
@@ -0,0 +1,5 @@
+# Faça um Programa que converta metros para centímetros.
+
+valor_metros = float(input('Digite o valor em metros: '))
+
+print('{} m é igual {:.2f} cm'.format(valor_metros, valor_metros * 100))
